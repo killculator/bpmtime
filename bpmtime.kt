8c45f562cdfcc1904148a895b2118a38b9b62371
killculator <axelexp2@gmail.com>
2018-06-27 20:07:55 -0500

    working, funcitonal (I think) bpm to ms conversion

--- bpmtime.kt
+++ bpmtime.kt
@@ -2,6 +2,11 @@ package org.killculator.bmptime
 
 fun main(args: Array<String>) {
 	val bpm: Int = args[0].toInt()
-	val ms: Double = 60000/bpm
-	println(ms)
+	val ms: Int = 60000/bpm
+
+	println("Bar:   $ms ms")
+	for (i in 1..7){
+		val divisor: Int = Math.pow(2.0, i.toDouble()).toInt()
+		println("1/${divisor}:   ${ms/(i*2)} ms")
+	}
 }
